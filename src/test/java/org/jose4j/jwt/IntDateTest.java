27aec5b705a80c4e6055fbc40a32c44b9d2034b4
Brian Campbell <brian.d.campbell@gmail.com>
2012-10-25 15:19:57 -0700

    test for IntDate

--- src/test/java/org/jose4j/jwt/IntDateTest.java
+++ src/test/java/org/jose4j/jwt/IntDateTest.java
@@ -6,4 +6,39 @@ import junit.framework.TestCase;
  */
 public class IntDateTest extends TestCase
 {
+    public void testBeforeAfterEquals1()
+    {
+        IntDate one = IntDate.fromSeconds(1350647028);
+        IntDate two = IntDate.fromSeconds(1350647029);
+        assertTrue(one.before(two));
+        assertFalse(two.before(one));
+        assertFalse(one.after(two));
+        assertTrue(two.after(one));
+        assertFalse(one.equals(two));
+    }
+
+    public void testBeforeAfterEquals2()
+    {
+        IntDate one = IntDate.fromSeconds(1350647028);
+        IntDate two = IntDate.fromSeconds(1350647028);
+        assertFalse(one.before(two));
+        assertFalse(two.before(one));
+        assertFalse(one.after(two));
+        assertFalse(two.after(one));
+        assertTrue(one.equals(two));
+    }
+
+    public void testEquals()
+    {
+        IntDate date1 = IntDate.now();
+        IntDate date2 = date1;
+        assertTrue(date1.equals(date2));
+        assertTrue(date2.equals(date1));
+        date2 = IntDate.fromSeconds(date1.getValue());
+        assertTrue(date1.equals(date2));
+        assertTrue(date2.equals(date1));
+        date2.addSeconds(100);
+        assertFalse(date1.equals(date2));
+        assertFalse(date2.equals(date1));
+    }
 }
