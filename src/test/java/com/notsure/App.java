75c3ff5436678af9549f025f27a27a245688ba0e
Brian Campbell <brian.d.campbell@gmail.com>
2012-10-31 09:01:07 -0600

    Apache License, Version 2.0

--- src/test/java/com/notsure/App.java
+++ src/test/java/com/notsure/App.java
@@ -1,11 +1,25 @@
+/*
+ * Copyright 2012 Brian Campbell
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.notsure;
 
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.codec.binary.StringUtils;
 import org.apache.commons.codec.binary.Hex;
 import org.jose4j.lang.ByteUtil;
-import org.jose4j.lang.JoseException;
-import org.jose4j.keys.ExampleRsaKeyFromJws;
 import org.jose4j.keys.BigEndianBigInteger;
 import org.jose4j.jwk.JsonWebKeySet;
 import org.jose4j.jwk.JsonWebKey;
@@ -15,7 +29,6 @@ import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 import java.util.Arrays;
 import java.security.*;
-import java.security.interfaces.RSAPublicKey;
 import java.security.spec.RSAPrivateKeySpec;
 import java.security.spec.KeySpec;
 import java.security.spec.InvalidKeySpecException;
