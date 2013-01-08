c5732cdf582af1cd4e159a36e7d4874753d05c28
Brian Campbell <brian.d.campbell@gmail.com>
2013-01-08 09:20:49 -0700

    clean up some stuff that wasn't being used

--- src/test/java/org/jose4j/jwa/AlgorithmFactoryTest.java
+++ src/test/java/org/jose4j/jwa/AlgorithmFactoryTest.java
@@ -24,11 +24,6 @@ import org.jose4j.lang.JoseException;
  */
 public class AlgorithmFactoryTest extends TestCase
 {
-    public void testTest()
-    {
-        AlgorithmFactory af2 = new AlgorithmFactory("jws-algorithms.properties");
-    }
-
     public void testAllJwsKeyTypesNotNull() throws JoseException
     {
         AlgorithmFactoryFactory algoFactoryFactory = AlgorithmFactoryFactory.getInstance();
