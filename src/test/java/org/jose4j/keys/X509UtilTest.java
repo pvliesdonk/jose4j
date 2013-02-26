6e119746d7ad664c762f2c604237ed1299485a80
Brian Campbell <brian.d.campbell@gmail.com>
2013-02-26 16:12:41 -0700

    add a trivial test for the x509util

---
+++ src/test/java/org/jose4j/keys/X509UtilTest.java
@@ -0,0 +1,34 @@
+package org.jose4j.keys;
+
+import junit.framework.TestCase;
+import org.jose4j.lang.JoseException;
+
+import java.security.cert.X509Certificate;
+
+/**
+ */
+public class X509UtilTest extends TestCase
+{
+    public void testFromBase64Der() throws JoseException
+    {
+        String s =
+                "MIICUTCCAfugAwIBAgIBADANBgkqhkiG9w0BAQQFADBXMQswCQYDVQQGEwJDTjEL\n" +
+                "MAkGA1UECBMCUE4xCzAJBgNVBAcTAkNOMQswCQYDVQQKEwJPTjELMAkGA1UECxMC\n" +
+                "VU4xFDASBgNVBAMTC0hlcm9uZyBZYW5nMB4XDTA1MDcxNTIxMTk0N1oXDTA1MDgx\n" +
+                "NDIxMTk0N1owVzELMAkGA1UEBhMCQ04xCzAJBgNVBAgTAlBOMQswCQYDVQQHEwJD\n" +
+                "TjELMAkGA1UEChMCT04xCzAJBgNVBAsTAlVOMRQwEgYDVQQDEwtIZXJvbmcgWWFu\n" +
+                "ZzBcMA0GCSqGSIb3DQEBAQUAA0sAMEgCQQCp5hnG7ogBhtlynpOS21cBewKE/B7j\n" +
+                "V14qeyslnr26xZUsSVko36ZnhiaO/zbMOoRcKK9vEcgMtcLFuQTWDl3RAgMBAAGj\n" +
+                "gbEwga4wHQYDVR0OBBYEFFXI70krXeQDxZgbaCQoR4jUDncEMH8GA1UdIwR4MHaA\n" +
+                "FFXI70krXeQDxZgbaCQoR4jUDncEoVukWTBXMQswCQYDVQQGEwJDTjELMAkGA1UE\n" +
+                "CBMCUE4xCzAJBgNVBAcTAkNOMQswCQYDVQQKEwJPTjELMAkGA1UECxMCVU4xFDAS\n" +
+                "BgNVBAMTC0hlcm9uZyBZYW5nggEAMAwGA1UdEwQFMAMBAf8wDQYJKoZIhvcNAQEE\n" +
+                "BQADQQA/ugzBrjjK9jcWnDVfGHlk3icNRq0oV7Ri32z/+HQX67aRfgZu7KWdI+Ju\n" +
+                "Wm7DCfrPNGVwFWUQOmsPue9rZBgO\n";
+
+        X509Util x5u = new X509Util();
+        X509Certificate x509Certificate = x5u.fromBase64Der(s);
+        assertTrue(x509Certificate.getSubjectDN().toString().contains("Yang"));
+    }
+
+}
