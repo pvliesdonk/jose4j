07e015f4f7617d39bb124e322cfb3f8b0cb06fde
Brian Campbell <brian.d.campbell@gmail.com>
2013-07-27 15:28:00 -0400

    working on wrapping my head around JWE. Got a text/example round trip of Example JWE using RSAES-PKCS1-V1_5 and AES_128_CBC_HMAC_SHA_256 working http://tools.ietf.org/html/draft-ietf-jose-json-web-encryption-13#appendix-A.2

--- src/test/java/com/notsure/App.java
+++ src/test/java/com/notsure/App.java
@@ -24,6 +24,7 @@ import org.jose4j.jwk.*;
 import org.jose4j.jws.EcdsaUsingShaAlgorithm;
 import org.jose4j.jws.JsonWebSignature;
 import org.jose4j.jws.AlgorithmIdentifiers;
+import org.jose4j.jwx.CompactSerialization;
 import org.jose4j.keys.*;
 import org.jose4j.lang.ByteUtil;
 import org.jose4j.lang.JoseException;
@@ -36,6 +37,7 @@ import org.json.simple.JSONObject;
 
 import javax.crypto.Cipher;
 import javax.crypto.Mac;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.ByteArrayInputStream;
 import java.math.BigInteger;
@@ -55,7 +57,6 @@ import java.util.LinkedHashMap;
 
 /**
  * Hello world!
- *
  */
 public class App 
 {
@@ -78,65 +79,27 @@ public class App
         String encodedHeader = b64.base64UrlEncodeUtf8ByteRepresentation(jweHeaderString);
         System.out.println(encodedHeader.equals("eyJhbGciOiJSU0ExXzUiLCJlbmMiOiJBMTI4Q0JDLUhTMjU2In0"));
 
-        PublicJsonWebKey jwk = PublicJsonWebKey.Factory.newPublicJwk("{\"kty\":\"RSA\",\n" +
-                "      \"n\":\"sXchDaQebHnPiGvyDOAT4saGEUetSyo9MKLOoWFsueri23bOdgWp4Dy1Wl\n" +
-                "           UzewbgBHod5pcM9H95GQRV3JDXboIRROSBigeC5yjU1hGzHHyXss8UDpre\n" +
-                "           cbAYxknTcQkhslANGRUZmdTOQ5qTRsLAt6BTYuyvVRdhS8exSZEy_c4gs_\n" +
-                "           7svlJJQ4H9_NxsiIoLwAEk7-Q3UXERGYw_75IDrGA84-lA_-Ct4eTlXHBI\n" +
-                "           Y2EaV7t7LjJaynVJCpkv4LKjTTAumiGUIuQhrNhZLuF_RJLqHpM2kgWFLU\n" +
-                "           7-VTdL1VbC2tejvcI2BlMkEpk1BzBZI0KQB0GaDWFLN-aEAw3vRw\",\n" +
-                "      \"e\":\"AQAB\",\n" +
-                "      \"d\":\"VFCWOqXr8nvZNyaaJLXdnNPXZKRaWCjkU5Q2egQQpTBMwhprMzWzpR8Sxq\n" +
-                "           1OPThh_J6MUD8Z35wky9b8eEO0pwNS8xlh1lOFRRBoNqDIKVOku0aZb-ry\n" +
-                "           nq8cxjDTLZQ6Fz7jSjR1Klop-YKaUHc9GsEofQqYruPhzSA-QgajZGPbE_\n" +
-                "           0ZaVDJHfyd7UUBUKunFMScbflYAAOYJqVIVwaYR5zWEEceUjNnTNo_CVSj\n" +
-                "           -VvXLO5VZfCUAVLgW4dpf1SrtZjSt34YLsRarSb127reG_DUwg9Ch-Kyvj\n" +
-                "           T1SkHgUWRVGcyly7uvVGRSDwsXypdrNinPA4jlhoNdizK2zF2CWQ\"\n" +
-                "     }");
-
-
         Cipher rsa15cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
-        rsa15cipher.init(Cipher.WRAP_MODE, jwk.getPublicKey());
+        rsa15cipher.init(Cipher.WRAP_MODE, ExampleRsaJwksFromJwe.APPENDIX_A_2.getPublicKey());
 
         byte[] contentEncryptionKeyBytes = ByteUtil.convertUnsignedToSignedTwosComp(new int[]{4, 211, 31, 197, 84, 157, 252, 254, 11, 100, 157, 250, 63, 170, 106, 206, 107, 124, 212, 45, 111, 107, 9, 219, 200, 177, 0, 240, 143, 156, 44, 207});
+        System.out.println("contentEncryptionKeyBytes bytes " + Arrays.toString(contentEncryptionKeyBytes));
+        System.out.println("contentEncryptionKeyBytes length " +contentEncryptionKeyBytes.length);
         SecretKeySpec contentEncryptionKey = new SecretKeySpec(contentEncryptionKeyBytes, "AES");
         byte[] encryptedCekBytes = rsa15cipher.wrap(contentEncryptionKey);
         System.out.println("encrypted key bytes " + Arrays.toString(encryptedCekBytes));
         System.out.println("encrypted key bytes length " +encryptedCekBytes.length);
 
-        int[] exkey = {80, 104, 72, 58, 11, 130, 236, 139, 132, 189, 255, 205, 61, 86, 151,
-           176, 99, 40, 44, 233, 176, 189, 205, 70, 202, 169, 72, 40, 226, 181,
-           156, 223, 120, 156, 115, 232, 150, 209, 145, 133, 104, 112, 237, 156,
-           116, 250, 65, 102, 212, 210, 103, 240, 177, 61, 93, 40, 71, 231, 223,
-           226, 240, 157, 15, 31, 150, 89, 200, 215, 198, 203, 108, 70, 117, 66,
-           212, 238, 193, 205, 23, 161, 169, 218, 243, 203, 128, 214, 127, 253,
-           215, 139, 43, 17, 135, 103, 179, 220, 28, 2, 212, 206, 131, 158, 128,
-           66, 62, 240, 78, 186, 141, 125, 132, 227, 60, 137, 43, 31, 152, 199,
-           54, 72, 34, 212, 115, 11, 152, 101, 70, 42, 219, 233, 142, 66, 151,
-           250, 126, 146, 141, 216, 190, 73, 50, 177, 146, 5, 52, 247, 28, 197,
-           21, 59, 170, 247, 181, 89, 131, 241, 169, 182, 246, 99, 15, 36, 102,
-           166, 182, 172, 197, 136, 230, 120, 60, 58, 219, 243, 149, 94, 222,
-           150, 154, 194, 110, 227, 225, 112, 39, 89, 233, 112, 207, 211, 241,
-           124, 174, 69, 221, 179, 107, 196, 225, 127, 167, 112, 226, 12, 242,
-           16, 24, 28, 120, 182, 244, 213, 244, 153, 194, 162, 69, 160, 244,
-           248, 63, 165, 141, 4, 207, 249, 193, 79, 131, 0, 169, 233, 127, 167,
-           101, 151, 125, 56, 112, 111, 248, 29, 232, 90, 29, 147, 110, 169,
-           146, 114, 165, 204, 71, 136, 41, 252};
-        System.out.println(exkey.length);
-
-
         String encodedJweEncryptedKey = b64.base64UrlEncode(encryptedCekBytes);
-        System.out.println(encodedJweEncryptedKey);
+        System.out.println("encodedJweEncryptedKey: " +encodedJweEncryptedKey);
 
         Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
 
         byte[] fullCekBytes = contentEncryptionKey.getEncoded();
 
-        byte[] hmacKeyBytes = new byte[fullCekBytes.length/2];
-        byte[] encKeyBytes = new byte[fullCekBytes.length/2];
-        System.arraycopy(fullCekBytes, 0, hmacKeyBytes, 0, hmacKeyBytes.length);
+        byte[] hmacKeyBytes = ByteUtil.leftHalf(fullCekBytes);
+        byte[] encKeyBytes = ByteUtil.rightHalf(fullCekBytes);
         System.out.println(Arrays.toString(hmacKeyBytes));
-        System.arraycopy(fullCekBytes, hmacKeyBytes.length, encKeyBytes, 0, encKeyBytes.length);
         System.out.println(Arrays.toString(encKeyBytes));
 
         System.out.println(Arrays.toString(contentEncryptionKeyBytes));
@@ -153,10 +116,116 @@ public class App
         byte[] aad = StringUtil.getBytesAscii(encodedHeader);
         System.out.println(Arrays.toString(aad));
 
-        MacUtil.getMac(MacUtil.HMAC_SHA256);
+        Mac mac = MacUtil.getMac(MacUtil.HMAC_SHA256);
+        mac.init(new HmacKey(hmacKeyBytes));
 
+//        4.  The octet string AL is equal to the number of bits in A expressed
+//            as a 64-bit unsigned integer in network byte order.
+
+        long alvalue = aad.length * 8;
+        byte[] al = ByteUtil.getBytes(alvalue);
+
+
+
+
+
+//        5.  A message authentication tag T is computed by applying HMAC
+//            [RFC2104] to the following data, in order:
+//
+//               the associated data A,
+//
+//               the initialization vector IV,
+//
+//               the ciphertext E computed in the previous step, and
+//
+//               the octet string AL defined above.
+
+
+        byte[] authenticationTagInput = ByteUtil.concat(aad, iv, cipherText, al);
+        byte[] authenticationTag = mac.doFinal(authenticationTagInput);
+        byte[] truncatedAuthenticationTag = ByteUtil.leftHalf(authenticationTag);
+
+        String encodedAuthenticationTag = b64.base64UrlEncode(truncatedAuthenticationTag);
+        String encodedCipherText = b64.base64UrlEncode(cipherText);
+
+        String cs = CompactSerialization.serialize(encodedHeader, encodedJweEncryptedKey, encodedJweInitializationVector, encodedCipherText, encodedAuthenticationTag);
+
+        decJweAppA2(cs);
+
+
+        String csFromAppendixA2 = "eyJhbGciOiJSU0ExXzUiLCJlbmMiOiJBMTI4Q0JDLUhTMjU2In0." +
+                "UGhIOguC7IuEvf_NPVaXsGMoLOmwvc1GyqlIKOK1nN94nHPoltGRhWhw7Zx0-kFm" +
+                "1NJn8LE9XShH59_i8J0PH5ZZyNfGy2xGdULU7sHNF6Gp2vPLgNZ__deLKxGHZ7Pc" +
+                "HALUzoOegEI-8E66jX2E4zyJKx-YxzZIItRzC5hlRirb6Y5Cl_p-ko3YvkkysZIF" +
+                "NPccxRU7qve1WYPxqbb2Yw8kZqa2rMWI5ng8OtvzlV7elprCbuPhcCdZ6XDP0_F8" +
+                "rkXds2vE4X-ncOIM8hAYHHi29NX0mcKiRaD0-D-ljQTP-cFPgwCp6X-nZZd9OHBv" +
+                "-B3oWh2TbqmScqXMR4gp_A." +
+                "AxY8DCtDaGlsbGljb3RoZQ." +
+                "KDlTtXchhZTGufMYmOYGS4HffxPSUrfmqCHXaI9wOGY." +
+                "9hH0vgRfYgPnAHOd8stkvw";
+
+        decJweAppA2(csFromAppendixA2);
 
     }
+
+    public static void decJweAppA2(String cs) throws JoseException, Exception
+    {
+        System.out.println("++++ decrypt +++++");
+        System.out.println(cs);
+
+        String[] deserialized = CompactSerialization.deserialize(cs);
+        String encodedHeader = deserialized[0];
+        String encodedJweEncryptedKey = deserialized[1];
+        String encodedJweInitializationVector = deserialized[2];
+        String encodedCipherText = deserialized[3];
+        String encodedAuthenticationTag = deserialized[4];
+
+        Cipher rsa15cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        rsa15cipher.init(Cipher.UNWRAP_MODE, ExampleRsaJwksFromJwe.APPENDIX_A_2.getPrivateKey());
+        
+        Base64Url base64Url = new Base64Url();
+        byte[] encryptedKeyBytes = base64Url.base64UrlDecode(encodedJweEncryptedKey);
+        System.out.println("encodedJweEncryptedKey: " +encodedJweEncryptedKey);
+
+        Key key = rsa15cipher.unwrap(encryptedKeyBytes, "AES", Cipher.SECRET_KEY);
+        byte[] contentEncryptionKeyBytes = key.getEncoded();
+        System.out.println("contentEncryptionKeyBytes getFormat() " + key.getFormat() + " getAlgorithm() " + key.getAlgorithm());
+
+        System.out.println("contentEncryptionKeyBytes bytes " + Arrays.toString(contentEncryptionKeyBytes));
+        System.out.println("contentEncryptionKeyBytes length " +contentEncryptionKeyBytes.length);
+
+        byte[] hmacKeyBytes = ByteUtil.leftHalf(contentEncryptionKeyBytes);
+        byte[] encKeyBytes = ByteUtil.rightHalf(contentEncryptionKeyBytes);
+
+        Mac mac = MacUtil.getMac(MacUtil.HMAC_SHA256);
+        mac.init(new HmacKey(hmacKeyBytes));
+
+        byte[] cipherText = base64Url.base64UrlDecode(encodedCipherText);
+        byte[] iv = base64Url.base64UrlDecode(encodedJweInitializationVector);
+
+//        Let the Additional Authenticated Data encryption parameter be
+//                the octets of the ASCII representation of the Encoded JWE Header
+//                value.
+        byte[] aad = StringUtil.getBytesAscii(encodedHeader);
+        long numAadBits = 8 * aad.length;
+        byte[] al = ByteUtil.getBytes(numAadBits);
+
+        byte[] authenticationTagInput = ByteUtil.concat(aad, iv, cipherText, al);
+        byte[] calculatedAuthenticationTag = mac.doFinal(authenticationTagInput);
+        calculatedAuthenticationTag = ByteUtil.subArray(calculatedAuthenticationTag, 0, 16);
+        byte[] authenticationTag = base64Url.base64UrlDecode(encodedAuthenticationTag);
+        boolean tagMatch = ByteUtil.secureEquals(authenticationTag, calculatedAuthenticationTag);
+        System.out.println(tagMatch);
+
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(encKeyBytes, "AES"), new IvParameterSpec(iv));
+
+        byte[] bytes = cipher.doFinal(cipherText);
+        String plainText = StringUtil.newStringUtf8(bytes);
+
+        System.out.println(plainText);
+    }
+
 
     public static void mainForCISPreso(String... args) throws JoseException
     {
