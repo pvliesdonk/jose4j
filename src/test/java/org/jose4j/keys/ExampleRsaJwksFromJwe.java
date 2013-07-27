e358892015f25c4813f7a4aa61333b176781940a
Brian Campbell <brian.d.campbell@gmail.com>
2013-07-27 15:19:16 -0400

    again with the key (rsa jwk from jwe example)

--- src/test/java/org/jose4j/keys/ExampleRsaJwksFromJwe.java
+++ src/test/java/org/jose4j/keys/ExampleRsaJwksFromJwe.java
@@ -1,7 +1,40 @@
 package org.jose4j.keys;
 
+import org.jose4j.jwk.PublicJsonWebKey;
+import org.jose4j.lang.JoseException;
+import org.jose4j.lang.UncheckedJoseException;
+
 /**
  */
 public class ExampleRsaJwksFromJwe
 {
+    public static PublicJsonWebKey APPENDIX_A_2;
+
+    static
+    {
+        try
+        {
+            APPENDIX_A_2 = PublicJsonWebKey.Factory.newPublicJwk("{\"kty\":\"RSA\",\n" +
+                "      \"n\":\"sXchDaQebHnPiGvyDOAT4saGEUetSyo9MKLOoWFsueri23bOdgWp4Dy1Wl\n" +
+                "           UzewbgBHod5pcM9H95GQRV3JDXboIRROSBigeC5yjU1hGzHHyXss8UDpre\n" +
+                "           cbAYxknTcQkhslANGRUZmdTOQ5qTRsLAt6BTYuyvVRdhS8exSZEy_c4gs_\n" +
+                "           7svlJJQ4H9_NxsiIoLwAEk7-Q3UXERGYw_75IDrGA84-lA_-Ct4eTlXHBI\n" +
+                "           Y2EaV7t7LjJaynVJCpkv4LKjTTAumiGUIuQhrNhZLuF_RJLqHpM2kgWFLU\n" +
+                "           7-VTdL1VbC2tejvcI2BlMkEpk1BzBZI0KQB0GaDWFLN-aEAw3vRw\",\n" +
+                "      \"e\":\"AQAB\",\n" +
+                "      \"d\":\"VFCWOqXr8nvZNyaaJLXdnNPXZKRaWCjkU5Q2egQQpTBMwhprMzWzpR8Sxq\n" +
+                "           1OPThh_J6MUD8Z35wky9b8eEO0pwNS8xlh1lOFRRBoNqDIKVOku0aZb-ry\n" +
+                "           nq8cxjDTLZQ6Fz7jSjR1Klop-YKaUHc9GsEofQqYruPhzSA-QgajZGPbE_\n" +
+                "           0ZaVDJHfyd7UUBUKunFMScbflYAAOYJqVIVwaYR5zWEEceUjNnTNo_CVSj\n" +
+                "           -VvXLO5VZfCUAVLgW4dpf1SrtZjSt34YLsRarSb127reG_DUwg9Ch-Kyvj\n" +
+                "           T1SkHgUWRVGcyly7uvVGRSDwsXypdrNinPA4jlhoNdizK2zF2CWQ\"\n" +
+                "     }");
+        }
+        catch (JoseException e)
+        {
+            throw new UncheckedJoseException("This is bad...", e);
+        }
+    }
+
+
 }
