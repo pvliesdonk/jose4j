59dec343ba9ca828003d4af23a460c4acb215e58
Brian Campbell <brian.d.campbell@gmail.com>
2013-08-01 08:58:42 +0200

    introduce a base RsaKeyManagementAlgorithm, have 1.5 extend it and add one for OAEP. That last part might need some work but it's a start.

--- src/main/java/org/jose4j/jwe/Rsa1_5KeyManagementAlgorithm.java
+++ src/main/java/org/jose4j/jwe/Rsa1_5KeyManagementAlgorithm.java
@@ -14,72 +14,11 @@ import java.security.Key;
 
 /**
  */
-public class Rsa1_5KeyManagementAlgorithm extends AlgorithmInfo implements KeyManagementAlgorithm
+public class Rsa1_5KeyManagementAlgorithm extends RsaKeyManagementAlgorithm implements KeyManagementAlgorithm
 {
     public Rsa1_5KeyManagementAlgorithm()
     {
-        setJavaAlgorithm("RSA/ECB/PKCS1Padding");
-        setAlgorithmIdentifier(KeyManagementAlgorithmIdentifiers.RSA1_5);
-        setKeyType(RsaJsonWebKey.KEY_TYPE);
-        setKeyPersuasion(KeyPersuasion.ASYMMETRIC);
-    }
+        super("RSA/ECB/PKCS1Padding", KeyManagementAlgorithmIdentifiers.RSA1_5);
 
-    public ContentEncryptionKeys manageForEncrypt(Key managementKey, ContentEncryptionKeyDescriptor cekDesc) throws JoseException
-    {
-        Cipher cipher = CipherUtil.getCipher(getJavaAlgorithm());
-        byte[] contentEncryptionKey = ByteUtil.randomBytes(cekDesc.getContentEncryptionKeyByteLength());
-
-        try
-        {
-            cipher.init(Cipher.WRAP_MODE, managementKey);
-            String contentEncryptionKeyAlgorithm = cekDesc.getContentEncryptionKeyAlgorithm();
-            byte[] encryptedKey = cipher.wrap(new SecretKeySpec(contentEncryptionKey, contentEncryptionKeyAlgorithm));
-            return new ContentEncryptionKeys(contentEncryptionKey, encryptedKey);
-        }
-        catch (IllegalBlockSizeException e)
-        {
-            throw new JoseException("Unable to encrypt the Content Encryption Key", e);
-        }
-        catch (InvalidKeyException e)
-        {
-            throw new JoseException("Unable to encrypt the Content Encryption Key", e);
-        }
-    }
-
-    public Key manageForDecrypt(Key managementKey, byte[] encryptedKey, ContentEncryptionKeyDescriptor cekDesc) throws JoseException
-    {
-        Cipher cipher = CipherUtil.getCipher(getJavaAlgorithm());
-
-        try
-        {
-            cipher.init(Cipher.UNWRAP_MODE, managementKey);
-        }
-        catch (InvalidKeyException e)
-        {
-            throw new JoseException("Unable to initialize cipher for key decryption", e);
-        }
-
-        String cekAlg = cekDesc.getContentEncryptionKeyAlgorithm();
-
-        try
-        {
-            return cipher.unwrap(encryptedKey, cekAlg, Cipher.SECRET_KEY);
-        }
-        catch (Exception e)
-        {
-            /*
-                TODO - is this good enough for step 10 from
-                http://tools.ietf.org/html/draft-ietf-jose-json-web-encryption-14#section-5.2
-                ?
-                    To mitigate the attacks described in RFC 3218 [RFC3218], the
-                    recipient MUST NOT distinguish between format, padding, and
-                    length errors of encrypted keys.  It is strongly recommended, in
-                    the event of receiving an improperly formatted key, that the
-                    receiver substitute a randomly generated CEK and proceed to the
-                    next step, to mitigate timing attacks.
-             */
-            byte[] bytes = ByteUtil.randomBytes(cekDesc.getContentEncryptionKeyByteLength());
-            return new SecretKeySpec(bytes, cekAlg);
-        }
     }
 }
