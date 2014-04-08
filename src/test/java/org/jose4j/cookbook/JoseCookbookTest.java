0a935f53934b70d657e585469bf007305355cc14
Brian Campbell <brian.d.campbell@gmail.com>
2014-04-08 04:46:06 -0600

    verify that we can decrypt cookbook 4.1 RSA v1.5 and AES-HMAC-SHA2

--- src/test/java/org/jose4j/cookbook/JoseCookbookTest.java
+++ src/test/java/org/jose4j/cookbook/JoseCookbookTest.java
@@ -17,6 +17,7 @@
 package org.jose4j.cookbook;
 
 import org.jose4j.base64url.Base64Url;
+import org.jose4j.jwe.JsonWebEncryption;
 import org.jose4j.jwk.JsonWebKey;
 import org.jose4j.jwk.PublicJsonWebKey;
 import org.jose4j.jws.AlgorithmIdentifiers;
@@ -46,8 +47,14 @@ public class JoseCookbookTest
             "lvdXIgZG9vci4gWW91IHN0ZXAgb250byB0aGUgcm9hZCwgYW5kIGlmIHlvdSBk" +
             "b24ndCBrZWVwIHlvdXIgZmVldCwgdGhlcmXigJlzIG5vIGtub3dpbmcgd2hlcm" +
             "UgeW91IG1pZ2h0IGJlIHN3ZXB0IG9mZiB0by4";
-
     String jwsPayload = Base64Url.decodeToUtf8String(encodedJwsPayload);
+
+    // http://tools.ietf.org/html/draft-ietf-jose-cookbook-01#section-4
+    String jwePlaintext = "You can trust us to stick with you through thick and " +
+            "thin–to the bitter end. And you can trust us to "+
+            "keep any secret of yours–closer than you keep it " +
+            "yourself. But you cannot trust us to let you face trouble " +
+            "alone, and go off without a word. We are your friends, Frodo.";
 
     @Test
     public void rsa_v1_5Signature_3_1() throws JoseException
@@ -269,5 +276,81 @@ public class JoseCookbookTest
         assertThat(encodedJwsPayload, is(equalTo(jws.getEncodedPayload())));
     }
 
+    @Test
+    public void encryptionRSAv1_5andAES_HMAC_SHA2_4_1() throws JoseException
+    {
+        String jwkJsonString =
+                "   {\n" +
+                        "     \"kty\": \"RSA\",\n" +
+                        "     \"kid\": \"frodo.baggins@hobbiton.example\",\n" +
+                        "     \"use\": \"enc\",\n" +
+                        "     \"n\": \"maxhbsmBtdQ3CNrKvprUE6n9lYcregDMLYNeTAWcLj8NnPU9XIYegT\n" +
+                        "         HVHQjxKDSHP2l-F5jS7sppG1wgdAqZyhnWvXhYNvcM7RfgKxqNx_xAHx\n" +
+                        "         6f3yy7s-M9PSNCwPC2lh6UAkR4I00EhV9lrypM9Pi4lBUop9t5fS9W5U\n" +
+                        "         NwaAllhrd-osQGPjIeI1deHTwx-ZTHu3C60Pu_LJIl6hKn9wbwaUmA4c\n" +
+                        "         R5Bd2pgbaY7ASgsjCUbtYJaNIHSoHXprUdJZKUMAzV0WOKPfA6OPI4oy\n" +
+                        "         pBadjvMZ4ZAj3BnXaSYsEZhaueTXvZB4eZOAjIyh2e_VOIKVMsnDrJYA\n" +
+                        "         VotGlvMQ\",\n" +
+                        "     \"e\": \"AQAB\",\n" +
+                        "     \"d\": \"Kn9tgoHfiTVi8uPu5b9TnwyHwG5dK6RE0uFdlpCGnJN7ZEi963R7wy\n" +
+                        "         bQ1PLAHmpIbNTztfrheoAniRV1NCIqXaW_qS461xiDTp4ntEPnqcKsyO\n" +
+                        "         5jMAji7-CL8vhpYYowNFvIesgMoVaPRYMYT9TW63hNM0aWs7USZ_hLg6\n" +
+                        "         Oe1mY0vHTI3FucjSM86Nff4oIENt43r2fspgEPGRrdE6fpLc9Oaq-qeP\n" +
+                        "         1GFULimrRdndm-P8q8kvN3KHlNAtEgrQAgTTgz80S-3VD0FgWfgnb1PN\n" +
+                        "         miuPUxO8OpI9KDIfu_acc6fg14nsNaJqXe6RESvhGPH2afjHqSy_Fd2v\n" +
+                        "         pzj85bQQ\",\n" +
+                        "     \"p\": \"2DwQmZ43FoTnQ8IkUj3BmKRf5Eh2mizZA5xEJ2MinUE3sdTYKSLtaE\n" +
+                        "         oekX9vbBZuWxHdVhM6UnKCJ_2iNk8Z0ayLYHL0_G21aXf9-unynEpUsH\n" +
+                        "         7HHTklLpYAzOOx1ZgVljoxAdWNn3hiEFrjZLZGS7lOH-a3QQlDDQoJOJ\n" +
+                        "         2VFmU\",\n" +
+                        "     \"q\": \"te8LY4-W7IyaqH1ExujjMqkTAlTeRbv0VLQnfLY2xINnrWdwiQ93_V\n" +
+                        "         F099aP1ESeLja2nw-6iKIe-qT7mtCPozKfVtUYfz5HrJ_XY2kfexJINb\n" +
+                        "         9lhZHMv5p1skZpeIS-GPHCC6gRlKo1q-idn_qxyusfWv7WAxlSVfQfk8\n" +
+                        "         d6Et0\",\n" +
+                        "     \"dp\": \"UfYKcL_or492vVc0PzwLSplbg4L3-Z5wL48mwiswbpzOyIgd2xHTH\n" +
+                        "         QmjJpFAIZ8q-zf9RmgJXkDrFs9rkdxPtAsL1WYdeCT5c125Fkdg317JV\n" +
+                        "         RDo1inX7x2Kdh8ERCreW8_4zXItuTl_KiXZNU5lvMQjWbIw2eTx1lpsf\n" +
+                        "         lo0rYU\",\n" +
+                        "     \"dq\": \"iEgcO-QfpepdH8FWd7mUFyrXdnOkXJBCogChY6YKuIHGc_p8Le9Mb\n" +
+                        "         pFKESzEaLlN1Ehf3B6oGBl5Iz_ayUlZj2IoQZ82znoUrpa9fVYNot87A\n" +
+                        "         CfzIG7q9Mv7RiPAderZi03tkVXAdaBau_9vs5rS-7HMtxkVrxSUvJY14\n" +
+                        "         TkXlHE\",\n" +
+                        "     \"qi\": \"kC-lzZOqoFaZCr5l0tOVtREKoVqaAYhQiqIRGL-MzS4sCmRkxm5vZ\n" +
+                        "         lXYx6RtE1n_AagjqajlkjieGlxTTThHD8Iga6foGBMaAr5uR1hGQpSc7\n" +
+                        "         Gl7CF1DZkBJMTQN6EshYzZfxW08mIO8M6Rzuh0beL6fG9mkDcIyPrBXx\n" +
+                        "         2bQ_mM\"\n" +
+                        "   }";
 
+        String jweCompactSerialization =
+                "eyJhbGciOiJSU0ExXzUiLCJraWQiOiJmcm9kby5iYWdnaW5zQGhvYmJpdG9uLm" +
+                "V4YW1wbGUiLCJlbmMiOiJBMTI4Q0JDLUhTMjU2In0" +
+                "." +
+                "laLxI0j-nLH-_BgLOXMozKxmy9gffy2gTdvqzfTihJBuuzxg0V7yk1WClnQePF" +
+                "vG2K-pvSlWc9BRIazDrn50RcRai__3TDON395H3c62tIouJJ4XaRvYHFjZTZ2G" +
+                "Xfz8YAImcc91Tfk0WXC2F5Xbb71ClQ1DDH151tlpH77f2ff7xiSxh9oSewYrcG" +
+                "TSLUeeCt36r1Kt3OSj7EyBQXoZlN7IxbyhMAfgIe7Mv1rOTOI5I8NQqeXXW8Vl" +
+                "zNmoxaGMny3YnGir5Wf6Qt2nBq4qDaPdnaAuuGUGEecelIO1wx1BpyIfgvfjOh" +
+                "MBs9M8XL223Fg47xlGsMXdfuY-4jaqVw" +
+                "." +
+                "bbd5sTkYwhAIqfHsx8DayA" +
+                "." +
+                "0fys_TY_na7f8dwSfXLiYdHaA2DxUjD67ieF7fcVbIR62JhJvGZ4_FNVSiGc_r" +
+                "aa0HnLQ6s1P2sv3Xzl1p1l_o5wR_RsSzrS8Z-wnI3Jvo0mkpEEnlDmZvDu_k8O" +
+                "WzJv7eZVEqiWKdyVzFhPpiyQU28GLOpRc2VbVbK4dQKPdNTjPPEmRqcaGeTWZV" +
+                "yeSUvf5k59yJZxRuSvWFf6KrNtmRdZ8R4mDOjHSrM_s8uwIFcqt4r5GX8TKaI0" +
+                "zT5CbL5Qlw3sRc7u_hg0yKVOiRytEAEs3vZkcfLkP6nbXdC_PkMdNS-ohP78T2" +
+                "O6_7uInMGhFeX4ctHG7VelHGiT93JfWDEQi5_V9UN1rhXNrYu-0fVMkZAKX3VW" +
+                "i7lzA6BP430m" +
+                "." +
+                "kvKuFBXHe5mQr4lqgobAUg";
+
+        PublicJsonWebKey jwk = PublicJsonWebKey.Factory.newPublicJwk(jwkJsonString);
+
+        // verify that we can decrypt it
+        JsonWebEncryption jwe = new JsonWebEncryption();
+        jwe.setCompactSerialization(jweCompactSerialization);
+        jwe.setKey(jwk.getPrivateKey());
+        assertThat(jwePlaintext, equalTo(jwe.getPlaintextString()));
+
+    }
 }
