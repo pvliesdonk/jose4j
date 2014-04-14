eb0c4ff4ce26fbcd7b8a53761d2d4c075677135c
Brian Campbell <brian.d.campbell@gmail.com>
2014-04-14 06:46:41 -0600

    add some debug logging to CipherStrengthSupport

--- src/main/java/org/jose4j/jwe/CipherStrengthSupport.java
+++ src/main/java/org/jose4j/jwe/CipherStrengthSupport.java
@@ -1,5 +1,7 @@
 package org.jose4j.jwe;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.jose4j.lang.ByteUtil;
 
 import javax.crypto.Cipher;
@@ -9,18 +11,28 @@ import java.security.NoSuchAlgorithmException;
  */
 public class CipherStrengthSupport
 {
+    private static Log log = LogFactory.getLog(CipherStrengthSupport.class);
+
     public static boolean isAvailable(String algorithm, int keyByteLength)
     {
+        boolean isAvailable;
         int bitKeyLength = ByteUtil.bitLength(keyByteLength);
         try
         {
             int maxAllowedKeyLength = Cipher.getMaxAllowedKeyLength(algorithm);
-            return (bitKeyLength <= maxAllowedKeyLength);
+            isAvailable = (bitKeyLength <= maxAllowedKeyLength);
+
+            if (!isAvailable)
+            {
+                log.debug("max allowed key length for " + algorithm + " is " + maxAllowedKeyLength);
+            }
         }
         catch (NoSuchAlgorithmException e)
         {
-            return false;
+            log.debug("Unknown/unsupported algorithm," + algorithm + " " + e);
+            isAvailable = false;
         }
+        return isAvailable;
     }
 
 }
