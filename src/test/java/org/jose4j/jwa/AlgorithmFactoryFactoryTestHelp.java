719ec658339416d6fffefcb36a03c47f55666d16
Brian Campbell <brian.d.campbell@gmail.com>
2014-04-15 13:27:38 -0600

    add a runWithBouncyCastleProvider test method

--- src/test/java/org/jose4j/jwa/AlgorithmFactoryFactoryTestHelp.java
+++ src/test/java/org/jose4j/jwa/AlgorithmFactoryFactoryTestHelp.java
@@ -1,12 +1,58 @@
 package org.jose4j.jwa;
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import java.security.Security;
+
 /**
  *
  */
 public class AlgorithmFactoryFactoryTestHelp
 {
-    public static void reinitialize()
+    private static void reinitialize()
     {
         AlgorithmFactoryFactory.getInstance().reinitialize();
+    }
+
+    public static void runWithBouncyCastleProvider(RunnableTest test)  throws Exception
+    {
+        runWithBouncyCastleProvider(test, true);
+    }
+
+
+    public static void runWithBouncyCastleProvider(RunnableTest test, boolean doReinitialize) throws Exception
+    {
+        BouncyCastleProvider bouncyCastleProvider = new BouncyCastleProvider();
+        boolean hasBouncyCastleAlready = Security.getProvider(bouncyCastleProvider.getName()) != null;
+
+        try
+        {
+            if (!hasBouncyCastleAlready)
+            {
+                Security.addProvider(bouncyCastleProvider);
+                if (doReinitialize)
+                {
+                    reinitialize();
+                }
+            }
+
+            test.runTest();
+        }
+        finally
+        {
+            if (!hasBouncyCastleAlready)
+            {
+                Security.removeProvider(bouncyCastleProvider.getName());
+                if (doReinitialize)
+                {
+                    reinitialize();
+                }
+            }
+        }
+    }
+
+    public static interface RunnableTest
+    {
+        public abstract void runTest() throws Exception;
     }
 }
