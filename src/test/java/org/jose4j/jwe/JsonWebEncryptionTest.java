aab5ada5d5e73b4d44e4b1c16fd5a8bc169e1467
Brian Campbell <brian.d.campbell@gmail.com>
2014-04-16 05:20:28 -0600

    add test to decrypt the whole Compact Serialization from JWE Appendix 1 now that we’ve got AES GCM content encryption http://tools.ietf.org/html/draft-ietf-jose-json-web-encryption-25#appendix-A.1

--- src/test/java/org/jose4j/jwe/JsonWebEncryptionTest.java
+++ src/test/java/org/jose4j/jwe/JsonWebEncryptionTest.java
@@ -28,12 +28,16 @@ import org.junit.Test;
 import javax.crypto.spec.SecretKeySpec;
 import java.security.Key;
 
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.jose4j.jwa.AlgorithmConstraints.ConstraintType.BLACKLIST;
 import static org.jose4j.jwa.AlgorithmConstraints.ConstraintType.WHITELIST;
+import static org.jose4j.jwa.JceProviderTestSupport.RunnableTest;
+import static org.jose4j.jwa.JceProviderTestSupport.runWithBouncyCastleProvider;
 import static org.jose4j.jwe.ContentEncryptionAlgorithmIdentifiers.AES_128_CBC_HMAC_SHA_256;
 import static org.jose4j.jwe.KeyManagementAlgorithmIdentifiers.DIRECT;
 import static org.jose4j.jwe.KeyManagementAlgorithmIdentifiers.RSA1_5;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 /**
  */
@@ -83,6 +87,37 @@ public class JsonWebEncryptionTest
         jwe.setCompactSerialization(jweCsFromAppendixA2);
         String plaintextString = jwe.getPlaintextString();
         assertEquals("Live long and prosper.", plaintextString);
+    }
+
+    @Test
+    public void jweExampleA1() throws Exception
+    {
+        // http://tools.ietf.org/html/draft-ietf-jose-json-web-encryption-25#appendix-A.1
+        runWithBouncyCastleProvider(new RunnableTest()
+        {
+            @Override
+            public void runTest() throws Exception
+            {
+                String cs =
+                        "eyJhbGciOiJSU0EtT0FFUCIsImVuYyI6IkEyNTZHQ00ifQ." +
+                        "OKOawDo13gRp2ojaHV7LFpZcgV7T6DVZKTyKOMTYUmKoTCVJRgckCL9kiMT03JGe" +
+                        "ipsEdY3mx_etLbbWSrFr05kLzcSr4qKAq7YN7e9jwQRb23nfa6c9d-StnImGyFDb" +
+                        "Sv04uVuxIp5Zms1gNxKKK2Da14B8S4rzVRltdYwam_lDp5XnZAYpQdb76FdIKLaV" +
+                        "mqgfwX7XWRxv2322i-vDxRfqNzo_tETKzpVLzfiwQyeyPGLBIO56YJ7eObdv0je8" +
+                        "1860ppamavo35UgoRdbYaBcoh9QcfylQr66oc6vFWXRcZ_ZT2LawVCWTIy3brGPi" +
+                        "6UklfCpIMfIjf7iGdXKHzg." +
+                        "48V1_ALb6US04U3b." +
+                        "5eym8TW_c8SuK0ltJ3rpYIzOeDQz7TALvtu6UG9oMo4vpzs9tX_EFShS8iB7j6ji" +
+                        "SdiwkIr3ajwQzaBtQD_A." +
+                        "XFBoMYUZodetZdvTiFvSkQ";
+
+                JsonWebEncryption jwe = new JsonWebEncryption();
+                jwe.setCompactSerialization(cs);
+                jwe.setKey(ExampleRsaJwksFromJwe.APPENDIX_A_1.getPrivateKey());
+                String examplePlaintext = "The true sign of intelligence is not knowledge but imagination.";
+                assertThat(examplePlaintext, equalTo(jwe.getPlaintextString()));
+            }
+        });
     }
 
     @Test
