7ea5a99faae6a04f5406c8e35ccc240eab8f902f
Brian Campbell <brian.d.campbell@gmail.com>
2014-08-19 14:51:48 -0600

    less superfluous stuff in a test json string

--- src/test/java/org/jose4j/json/JsonUtilTest.java
+++ src/test/java/org/jose4j/json/JsonUtilTest.java
@@ -54,18 +54,10 @@ public class JsonUtilTest extends TestCase
                 "  \"keys\": [\n" +
                 "    {\n" +
                 "      \"kty\": \"EC\",\n" +
-                "      \"kid\": \"20b06\",\n" +
-                "      \"use\": \"sig\",\n" +
-                "      \"x\": \"AXN9jWp9rpUE6XMxH3tsOZMbFRL-7beDHVCzWaZL-zUeHBaV48-oqRr0pnN7K6iyywqlYXCeqgDUsldpOcSCBB4A\",\n" +
-                "      \"y\": \"hKyRVd_GbKG7BIyoM09ZKtNDVJOtQTSZ9CX4wEqMDF1vXXFzrhEMsvB5E234gmMWBdKAnJW3-7NTTAFGmh39OXU\",\n" +
-                "      \"crv\": \"P-521\"\n" +
-                "    },\n" +
-                "    {\n" +
-                "      \"kty\": \"EC\",\n" +
                 "      \"kid\": \"20b05\",\n" +
                 "      \"use\": \"sig\",\n" +
-                "      \"x\": \"baLYEBtgwuj7YjAx6bhZ1nM_gZu8TgQQ8h6gNQ9IIiPNkxCOV-QDLjLcQ0USIor7\",\n" +
-                "      \"y\": \"Xh2QpnzdA9gz4aMaHDhSNl-MXeET54bylNZMkSqy1gDW17uhOH8FZBL6AB3GKQA1\",\n" +
+                "      \"x\": \"baLYE[omitted]DLSIor7\",\n" +
+                "      \"y\": \"Xh2Q4[omitted]AB3GKQ1\",\n" +
                 "      \"crv\": \"P-384\"\n" +
                 "    },\n" +
                 "    {\n" +
@@ -76,14 +68,6 @@ public class JsonUtilTest extends TestCase
                 "      \"y\": \"1FXTX9JGWH4kG0KxUIQDqOIxC2R8w5sLHHYr6sjcTK4\",\n" +
                 "      \"y\": \"1234567890abcdefghijklmnopqrstuvwxyzABCDEFG\",\n" +     // duplicate y
                 "      \"crv\": \"P-256\"\n" +
-                "    },\n" +
-                "    {\n" +
-                "      \"kty\": \"EC\",\n" +
-                "      \"kid\": \"20b02\",\n" +
-                "      \"use\": \"sig\",\n" +
-                "      \"x\": \"AaQZH0dX7tgq4nRq-f6LwlryxdTgEY0JTFoLwWZkgfQl0tD0uFhHAGhgR_c6bba4thZUIuvMDW-IIiilFb9ZfXQd\",\n" +
-                "      \"y\": \"AdDIPzaV4vagVlY-pXxhEmFuKJ171U-R0Wad2z-1yw_Ks2jWGYtqNr2B3qr-5N8xyBViplgKBfmB5BvBa2i-tVT6\",\n" +
-                "      \"crv\": \"P-521\"\n" +
                 "    }\n" +
                 "  ]\n" +
                 "}";
