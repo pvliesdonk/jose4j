e98b154db5e8800e3473d54f93eecdad6563b634
Brian Campbell <brian.d.campbell@gmail.com>
2015-01-27 10:57:12 -0700

    add HttpsJsonWebKeySet which can retrieve and cache JWKs from a JWKS HTTP(s) endpoint

---
+++ src/main/java/org/jose4j/jwk/HttpsJsonWebKeySet.java
@@ -0,0 +1,186 @@
+/*
+ * Copyright 2012-2015 Brian Campbell
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jose4j.jwk;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.jose4j.http.Get;
+import org.jose4j.http.SimpleGet;
+import org.jose4j.http.SimpleResponse;
+import org.jose4j.lang.JoseException;
+import org.jose4j.lang.UncheckedJoseException;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+/**
+ *
+ */
+public class HttpsJsonWebKeySet
+{
+    private static Log log = LogFactory.getLog(HttpsJsonWebKeySet.class);
+
+    private URL location;
+    private long defaultCacheDuration = 3600;
+    private SimpleGet simpleHttpGet = new Get();
+
+    private Cache cache = new Cache(Collections.<JsonWebKey>emptyList(), 0);
+
+    public HttpsJsonWebKeySet(String location)
+    {
+        try
+        {
+            this.location = new URL(location);
+        }
+        catch (MalformedURLException e)
+        {
+            throw new UncheckedJoseException("The location of the HTTPS JWKS endpoint ("+location+") could not be parsed into a URL.", e);
+        }
+    }
+
+    public void setDefaultCacheDuration(long defaultCacheDuration)
+    {
+        this.defaultCacheDuration = defaultCacheDuration;
+    }
+
+    public void setSimpleHttpGet(SimpleGet simpleHttpGet)
+    {
+        this.simpleHttpGet = simpleHttpGet;
+    }
+
+    public List<JsonWebKey> getJsonWebKeys() throws JoseException, IOException
+    {
+        if (cache.getExp() < System.currentTimeMillis())
+        {
+            refresh();
+        }
+        return cache.getKeys();
+    }
+
+    public void refresh() throws JoseException, IOException
+    {
+        if (log.isDebugEnabled()) {log.debug("Refreshing JWKS from " + location);}
+        SimpleResponse simpleResponse = simpleHttpGet.get(location);
+        JsonWebKeySet jwks = new JsonWebKeySet(simpleResponse.getBody());
+        List<JsonWebKey> keys = jwks.getJsonWebKeys();
+        long cacheLife = getCacheLife(simpleResponse);
+        if (cacheLife <= 0)
+        {
+            if (log.isDebugEnabled()) {log.debug("Will use default cache duration of "+defaultCacheDuration+" seconds for content from " + location);}
+            cacheLife = defaultCacheDuration;
+        }
+        long exp = System.currentTimeMillis() + (cacheLife * 1000L);
+        if (log.isDebugEnabled()) {log.debug("Updated JWKS content from " + location + " will be cached for " + cacheLife + " seconds until " + new Date(exp) + " -> " + keys);}
+        cache = new Cache(keys, exp);
+    }
+
+    static long getDateHeaderValue(SimpleResponse response, String headerName, long defaultValue)
+    {
+        List<String> values = getHeaderValues(response, headerName);
+        for (String value : values)
+        {
+            try
+            {
+                if (!value.endsWith("GMT"))
+                {
+                    value += " GMT";
+                }
+
+                return Date.parse(value);
+            }
+            catch (Exception e)
+            {
+                // ignore it
+            }
+        }
+        return defaultValue;
+    }
+
+    private static List<String> getHeaderValues(SimpleResponse response, String headerName)
+    {
+        List<String> values = response.getHeaderValues(headerName);
+        return  (values == null) ? Collections.<String>emptyList() : values;
+    }
+
+    static long getExpires(SimpleResponse response)
+    {
+        return getDateHeaderValue(response, "expires", 0);
+    }
+
+    static long getCacheLife(SimpleResponse response)
+    {
+        return getCacheLife(response, System.currentTimeMillis());
+    }
+
+    static long getCacheLife(SimpleResponse response, long currentTime)
+    {
+        // start with expires
+        long expires = getExpires(response);
+        long life = (expires - currentTime) / 1000L;
+
+        // but Cache-Control takes precedence
+        List<String> values = getHeaderValues(response, "cache-control");
+        for (String value : values)
+        {
+            try
+            {
+                // only care about the max-age value so just pull it out rather than parsing the whole header
+                value = (value == null) ? "" : value.toLowerCase();
+                int indexOfMaxAge = value.indexOf("max-age");
+                int indexOfComma = value.indexOf(',', indexOfMaxAge);
+                int end = indexOfComma == -1 ? value.length() : indexOfComma;
+                String part = value.substring(indexOfMaxAge, end);
+                part = part.substring(part.indexOf('=') + 1);
+                part = part.trim();
+                life = Long.parseLong(part);
+                break;
+            }
+            catch (Exception e)
+            {
+                // ignore it
+            }
+
+        }
+
+        return life;
+    }
+
+    private static class Cache
+    {
+        private List<JsonWebKey> keys;
+        private long exp;
+
+        private Cache(List<JsonWebKey> keys, long exp)
+        {
+            this.keys = keys;
+            this.exp = exp;
+        }
+
+        private List<JsonWebKey> getKeys()
+        {
+            return keys;
+        }
+
+        private long getExp()
+        {
+            return exp;
+        }
+    }
+}
---
+++ src/test/java/org/jose4j/jwk/HttpsJsonWebKeySetTest.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2012-2015 Brian Campbell
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jose4j.jwk;
+
+import org.jose4j.http.SimpleResponse;
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ *
+ */
+public class HttpsJsonWebKeySetTest
+{
+    @Test
+    public void testExpiresDateHeadersPerRfc() throws Exception
+    {
+        /*
+              3 different HTTP date formats per
+              http://tools.ietf.org/html/rfc7231#section-7.1.1.1  or
+              http://tools.ietf.org/html/rfc2616#section-3.3.1
+              Sun, 06 Nov 1994 08:49:37 GMT  ; RFC 822, updated by RFC 1123
+              Sunday, 06-Nov-94 08:49:37 GMT ; RFC 850, obsoleted by RFC 1036
+              Sun Nov  6 08:49:37 1994       ; ANSI C's asctime() format
+         */
+        long actualDateMs = 784111777000L;
+        long actualCacheLife = 60L;
+        long fakeCurrentTime = 784111717000L;
+
+        Map<String, List<String>> headers = Collections.singletonMap("Expires", Collections.singletonList("Sun, 06 Nov 1994 08:49:37 GMT"));
+        SimpleResponse simpleResponse = new SimpleResponse(200, "OK", headers, "doesn't matter");
+        assertThat(actualDateMs, equalTo(HttpsJsonWebKeySet.getExpires(simpleResponse)));
+        assertThat(actualCacheLife, equalTo(HttpsJsonWebKeySet.getCacheLife(simpleResponse, fakeCurrentTime)));
+
+        headers = Collections.singletonMap("Expires", Collections.singletonList("Sunday, 06-Nov-94 08:49:37 GMT"));
+        simpleResponse = new SimpleResponse(200, "OK", headers, "doesn't matter");
+        assertThat(actualDateMs, equalTo(HttpsJsonWebKeySet.getExpires(simpleResponse)));
+        assertThat(actualCacheLife, equalTo(HttpsJsonWebKeySet.getCacheLife(simpleResponse, fakeCurrentTime)));
+
+        headers = Collections.singletonMap("Expires", Collections.singletonList("Sun Nov  6 08:49:37 1994"));
+        simpleResponse = new SimpleResponse(200, "OK", headers, "*still* doesn't matter");
+        assertThat(actualDateMs, equalTo(HttpsJsonWebKeySet.getExpires(simpleResponse)));
+        assertThat(actualCacheLife, equalTo(HttpsJsonWebKeySet.getCacheLife(simpleResponse, fakeCurrentTime)));
+    }
+
+    @Test
+    public void testCacheLifeFromCacheControlMaxAge() throws Exception
+    {
+        String[] headerValues =
+        {
+            "public, max-age=23760, must-revalidate, no-transform",
+            "public, max-age=    23760 , must-revalidate",
+            "public,max-age = 23760, must-revalidate",
+            "public, max-age=23760, must-revalidate, no-transform",
+            "must-revalidate,public,max-age=23760,no-transform",
+            "max-age =23760, must-revalidate, public",
+            "max-age=23760",
+            "max-age =23760",
+            "max-age = 23760 ",
+            "max-age=23760,",
+            "fake=\"f,a,k,e\",public, max-age=23760, must-revalidate=\"this , shouldn't be here\", whatever",
+        };
+
+        for (String headerValue : headerValues)
+        {
+            Map<String, List<String>> headers = new HashMap<>();
+            headers.put("Expires", Collections.singletonList("Expires: Tue, 27 Jan 2015 16:00:10 GMT")); // Cache-Control takes precedence over this
+            headers.put("Cache-Control", Collections.singletonList(headerValue));
+            SimpleResponse simpleResponse = new SimpleResponse(200, "OK", headers, "doesn't matter");
+            long cacheLife = HttpsJsonWebKeySet.getCacheLife(simpleResponse);
+            assertThat("it done broke on this one " + headerValue, 23760L , equalTo(cacheLife));
+        }
+    }
+
+    // todo more tests
+}
