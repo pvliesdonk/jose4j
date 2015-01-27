cdf6064f5b9b70f7b9e687e2b2e92071f6e05a36
Brian Campbell <brian.d.campbell@gmail.com>
2015-01-27 10:23:26 -0700

    dump a couple half baked tests in there to maybe use bits of later...

--- src/test/java/com/notsure/App.java
+++ src/test/java/com/notsure/App.java
@@ -18,6 +18,7 @@ package com.notsure;
 
 import org.jose4j.base64url.Base64;
 import org.jose4j.base64url.Base64Url;
+import org.jose4j.http.Get;
 import org.jose4j.jwe.ContentEncryptionAlgorithmIdentifiers;
 import org.jose4j.jwe.JsonWebEncryption;
 import org.jose4j.jwe.KeyManagementAlgorithmIdentifiers;
@@ -29,15 +30,20 @@ import org.jose4j.jwx.HeaderParameterNames;
 import org.jose4j.keys.BigEndianBigInteger;
 import org.jose4j.keys.EllipticCurves;
 import org.jose4j.keys.PbkdfKey;
+import org.jose4j.keys.X509Util;
 import org.jose4j.lang.ByteUtil;
+import org.jose4j.lang.JoseException;
 import org.jose4j.lang.StringUtil;
 import org.jose4j.zip.CompressionAlgorithm;
 import org.jose4j.zip.CompressionAlgorithmIdentifiers;
+import org.junit.Test;
 
+import java.io.IOException;
 import java.security.MessageDigest;
 import java.security.Provider;
 import java.security.PublicKey;
 import java.security.Security;
+import java.security.cert.X509Certificate;
 import java.security.interfaces.RSAPublicKey;
 import java.util.LinkedList;
 import java.util.List;
@@ -47,6 +53,57 @@ import java.util.List;
  */
 public class App 
 {
+    public void smoething() throws IOException, JoseException
+    {
+        HttpsJsonWebKeySet hjwks = new HttpsJsonWebKeySet("https://www.googleapis.com/oauth2/v2/certs");
+        List<JsonWebKey> jsonWebKeys = hjwks.getJsonWebKeys();
+        System.out.println(jsonWebKeys);
+        VerificationJwkSelector vjs = new VerificationJwkSelector();
+        JsonWebSignature jws = new JsonWebSignature();
+        jws.setKeyIdHeaderValue("8472c6590b1778fe529c1bd3a8f181cc2af4b200"); // this changes...
+        jws.setAlgorithmHeaderValue(AlgorithmIdentifiers.RSA_USING_SHA256);
+        JsonWebKey select = vjs.select(jws, jsonWebKeys);
+        System.out.println(select);
+    }
+
+    public void testPFC() throws Exception
+    {
+        X509Util x509Util = new X509Util();
+        X509Certificate certificate = x509Util.fromBase64Der(
+                "MIICUDCCAbkCBETczdcwDQYJKoZIhvcNAQEFBQAwbzELMAkGA1UEBhMCVVMxCzAJ\n" +
+                        "BgNVBAgTAkNPMQ8wDQYDVQQHEwZEZW52ZXIxFTATBgNVBAoTDFBpbmdJZGVudGl0\n" +
+                        "eTEXMBUGA1UECxMOQnJpYW4gQ2FtcGJlbGwxEjAQBgNVBAMTCWxvY2FsaG9zdDAe\n" +
+                        "Fw0wNjA4MTExODM1MDNaFw0zMzEyMjcxODM1MDNaMG8xCzAJBgNVBAYTAlVTMQsw\n" +
+                        "CQYDVQQIEwJDTzEPMA0GA1UEBxMGRGVudmVyMRUwEwYDVQQKEwxQaW5nSWRlbnRp\n" +
+                        "dHkxFzAVBgNVBAsTDkJyaWFuIENhbXBiZWxsMRIwEAYDVQQDEwlsb2NhbGhvc3Qw\n" +
+                        "gZ8wDQYJKoZIhvcNAQEBBQADgY0AMIGJAoGBAJLrpeiY/Ai2gGFxNY8Tm/QSO8qg\n" +
+                        "POGKDMAT08QMyHRlxW8fpezfBTAtKcEsztPzwYTLWmf6opfJT+5N6cJKacxWchn/\n" +
+                        "dRrzV2BoNuz1uo7wlpRqwcaOoi6yHuopNuNO1ms1vmlv3POq5qzMe6c1LRGADyZh\n" +
+                        "i0KejDX6+jVaDiUTAgMBAAEwDQYJKoZIhvcNAQEFBQADgYEAMojbPEYJiIWgQzZc\n" +
+                        "QJCQeodtKSJl5+lA8MWBBFFyZmvZ6jUYglIQdLlc8Pu6JF2j/hZEeTI87z/DOT6U\n" +
+                        "uqZA83gZcy6re4wMnZvY2kWX9CsVWDCaZhnyhjBNYfhcOf0ZychoKShaEpTQ5UAG\n" +
+                        "wvYYcbqIWC04GAZYVsZxlPl9hoA=\n");
+
+
+        String location = "https://localhost:9031/pf/JWKS";
+
+        Get get = new Get();
+        get.setTrustedCertificates(certificate);
+        get.setReadTimeout(100);
+        get.setRetries(5);
+        get.setProgressiveRetryWait(true);
+        get.setInitialRetryWaitTime(500);
+
+        HttpsJsonWebKeySet httpsJwks = new HttpsJsonWebKeySet(location);
+        httpsJwks.setSimpleHttpGet(get);
+        httpsJwks.setDefaultCacheDuration(10);
+
+        while (true)
+        {
+            httpsJwks.getJsonWebKeys();
+        }
+    }
+
     public static void main(String... meh) throws Exception
     {
        String json = "     {\n" +
