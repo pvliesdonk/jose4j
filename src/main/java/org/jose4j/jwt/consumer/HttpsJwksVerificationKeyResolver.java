65c52d89a42cd833530aad950cb8d894c9ab59fe
Brian Campbell <brian.d.campbell@gmail.com>
2015-01-29 13:32:16 -0700

    create an HttpsJwksVerificationKeyResolver that basically does (or helps do) what is described in http://openid.net/specs/openid-connect-core-1_0.html#RotateSigKeys from the verifier/consumer perspective

---
+++ src/main/java/org/jose4j/jwt/consumer/HttpsJwksVerificationKeyResolver.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2012-2015 Brian Campbell
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jose4j.jwt.consumer;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.jose4j.jwk.HttpsJsonWebKeySet;
+import org.jose4j.jwk.JsonWebKey;
+import org.jose4j.jwk.VerificationJwkSelector;
+import org.jose4j.jws.JsonWebSignature;
+import org.jose4j.jwx.JsonWebStructure;
+import org.jose4j.lang.JoseException;
+import org.jose4j.lang.UnresolvableKeyException;
+
+import java.io.IOException;
+import java.security.Key;
+import java.util.List;
+
+/**
+ *
+ */
+public class HttpsJwksVerificationKeyResolver implements VerificationKeyResolver
+{
+    private Log log = LogFactory.getLog(this.getClass());
+
+    private HttpsJsonWebKeySet httpsJkws;
+
+    public HttpsJwksVerificationKeyResolver(HttpsJsonWebKeySet httpsJkws)
+    {
+        this.httpsJkws = httpsJkws;
+    }
+
+    @Override
+    public Key resolveKey(JsonWebSignature jws, List<JsonWebStructure> nestingContext) throws UnresolvableKeyException
+    {
+        JsonWebKey theChosenOne;
+        List<JsonWebKey> jsonWebKeys;
+
+        try
+        {
+            jsonWebKeys = httpsJkws.getJsonWebKeys();
+            VerificationJwkSelector verificationJwkSelector = new VerificationJwkSelector();
+
+            theChosenOne = verificationJwkSelector.select(jws, jsonWebKeys);
+            if (theChosenOne == null)
+            {
+                if (log.isDebugEnabled())
+                {
+                    StringBuilder sb = new StringBuilder();
+                    sb.append("Refreshing JWKs from ").append(httpsJkws.getLocation()).append(" as no suitable verification key for JWS w/ header ");
+                    sb.append(jws.getHeaders().getFullHeaderAsJsonString()).append(" was found in ").append(jsonWebKeys);
+                    log.debug(sb.toString());
+                }
+                httpsJkws.refresh();
+                jsonWebKeys = httpsJkws.getJsonWebKeys();
+                theChosenOne = verificationJwkSelector.select(jws, jsonWebKeys);
+            }
+        }
+        catch (JoseException | IOException e)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.append("Unable to find a suitable verification key for JWS w/ header ").append(jws.getHeaders().getFullHeaderAsJsonString());
+            sb.append(" due to an unexpected exception (").append(e).append(") while obtaining or using keys from JWKS endpoint at ").append(httpsJkws.getLocation());
+            throw new UnresolvableKeyException(sb.toString(), e);
+        }
+
+        if (theChosenOne == null)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.append("Unable to find a suitable verification key for JWS w/ header ").append(jws.getHeaders().getFullHeaderAsJsonString());
+            sb.append(" from JWKs ").append(jsonWebKeys).append(" obtained from ").append(httpsJkws.getLocation());
+            throw new UnresolvableKeyException(sb.toString());
+        }
+
+        return theChosenOne.getKey();
+    }
+}
---
+++ src/test/java/org/jose4j/jwt/consumer/HttpsJwksVerificationKeyResolverTest.java
@@ -0,0 +1,140 @@
+/*
+ * Copyright 2012-2015 Brian Campbell
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jose4j.jwt.consumer;
+
+import org.apache.commons.logging.LogFactory;
+import org.jose4j.http.Get;
+import org.jose4j.http.SimpleResponse;
+import org.jose4j.jwk.*;
+import org.jose4j.jws.AlgorithmIdentifiers;
+import org.jose4j.jws.JsonWebSignature;
+import org.jose4j.jwx.JsonWebStructure;
+import org.jose4j.lang.UnresolvableKeyException;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.URL;
+import java.security.Key;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+/**
+ *
+ */
+public class HttpsJwksVerificationKeyResolverTest
+{
+    @Test
+    public void simpleKeyFoundThenNotFoundAndRefreshToFindAndThenCantFind() throws Exception
+    {
+        String firstJkwsJson = "{\"keys\":[{\"kty\":\"EC\",\"kid\":\"k1\",\"x\":\"1u9oeAkLQJcAnrv_m4fupf-lF43yFqmNjMsrukKDhEE\",\"y\":\"RG0cyWzinUl8NpfVVw2DqfH6zRqU_yF6aL1swssNv4E\",\"crv\":\"P-256\"}]}";
+        String secondJwkJson = "{\"keys\":[{\"kty\":\"EC\",\"kid\":\"k2\",\"x\":\"865vGRGnwRFf1YWFI-ODhHkQwYs7dc9VlI8zleEUqyA\",\"y\":\"W-7d1hvHrhNqNGVVNZjTUopIdaegL3jEjWOPX284AOk\",\"crv\":\"P-256\"}]}";
+
+        JsonWebKeySet jwks = new JsonWebKeySet(firstJkwsJson);
+        JsonWebKey k1 = jwks.getJsonWebKeys().iterator().next();
+
+        jwks = new JsonWebKeySet(secondJwkJson);
+        JsonWebKey k2 = jwks.getJsonWebKeys().iterator().next();
+
+        String location = "https://www.example.org/";
+        URL url = new URL(location);
+        HttpsJsonWebKeySet httpsJkws = new HttpsJsonWebKeySet(location);
+
+        Get mockGet = mock(Get.class);
+        Map<String,List<String>> headers = Collections.emptyMap();
+        SimpleResponse ok1 = new SimpleResponse(200, "OK", headers, firstJkwsJson);
+        SimpleResponse ok2 = new SimpleResponse(200, "OK", headers, secondJwkJson);
+        when(mockGet.get(url)).thenReturn(ok1, ok2);
+
+        httpsJkws.setSimpleHttpGet(mockGet);
+
+        HttpsJwksVerificationKeyResolver resolver = new HttpsJwksVerificationKeyResolver(httpsJkws);
+        JsonWebSignature jws = new JsonWebSignature();
+        jws.setAlgorithmHeaderValue(AlgorithmIdentifiers.ECDSA_USING_P256_CURVE_AND_SHA256);
+        jws.setKeyIdHeaderValue("k1");
+        Key key = resolver.resolveKey(jws, Collections.<JsonWebStructure>emptyList());
+        assertThat(key, equalTo(k1.getKey()));
+
+        jws = new JsonWebSignature();
+        jws.setAlgorithmHeaderValue(AlgorithmIdentifiers.ECDSA_USING_P256_CURVE_AND_SHA256);
+        jws.setKeyIdHeaderValue("k1");
+        key = resolver.resolveKey(jws, Collections.<JsonWebStructure>emptyList());
+        assertThat(key, equalTo(k1.getKey()));
+
+        jws = new JsonWebSignature();
+        jws.setAlgorithmHeaderValue(AlgorithmIdentifiers.ECDSA_USING_P256_CURVE_AND_SHA256);
+        jws.setKeyIdHeaderValue("k1");
+        key = resolver.resolveKey(jws, Collections.<JsonWebStructure>emptyList());
+        assertThat(key, equalTo(k1.getKey()));
+
+        jws = new JsonWebSignature();
+        jws.setAlgorithmHeaderValue(AlgorithmIdentifiers.ECDSA_USING_P256_CURVE_AND_SHA256);
+        jws.setKeyIdHeaderValue("k2");
+        key = resolver.resolveKey(jws, Collections.<JsonWebStructure>emptyList());
+        assertThat(key, equalTo(k2.getKey()));
+
+        jws = new JsonWebSignature();
+        jws.setAlgorithmHeaderValue(AlgorithmIdentifiers.ECDSA_USING_P256_CURVE_AND_SHA256);
+        jws.setKeyIdHeaderValue("k2");
+        key = resolver.resolveKey(jws, Collections.<JsonWebStructure>emptyList());
+        assertThat(key, equalTo(k2.getKey()));
+
+        jws = new JsonWebSignature();
+        jws.setAlgorithmHeaderValue(AlgorithmIdentifiers.ECDSA_USING_P256_CURVE_AND_SHA256);
+        jws.setKeyIdHeaderValue("nope");
+        try
+        {
+            key = resolver.resolveKey(jws, Collections.<JsonWebStructure>emptyList());
+            fail("shouldn't have resolved a key but got " + key);
+        }
+        catch (UnresolvableKeyException e)
+        {
+            LogFactory.getLog(this.getClass()).debug("this was expected and is okay: " + e);
+            assertFalse("do you really need UnresolvableKeyException inside a UnresolvableKeyException?", e.getCause() instanceof UnresolvableKeyException);
+        }
+    }
+
+    @Test
+    public void testAnEx() throws Exception
+    {
+        String location = "https://www.example.org/";
+
+        Get mockGet = mock(Get.class);
+        when(mockGet.get(new URL(location))).thenThrow(new IOException(location + "says 'no GET for you!'"));
+        HttpsJsonWebKeySet httpsJkws = new HttpsJsonWebKeySet(location);
+        httpsJkws.setSimpleHttpGet(mockGet);
+        HttpsJwksVerificationKeyResolver resolver = new HttpsJwksVerificationKeyResolver(httpsJkws);
+
+        JsonWebSignature jws = new JsonWebSignature();
+        jws.setAlgorithmHeaderValue(AlgorithmIdentifiers.ECDSA_USING_P256_CURVE_AND_SHA256);
+        jws.setKeyIdHeaderValue("nope");
+        try
+        {
+            Key key = resolver.resolveKey(jws, Collections.<JsonWebStructure>emptyList());
+            fail("shouldn't have resolved a key but got " + key);
+
+        }
+        catch (UnresolvableKeyException e)
+        {
+            LogFactory.getLog(this.getClass()).debug("this was expected and is okay: " + e);
+        }
+    }
+
+}
